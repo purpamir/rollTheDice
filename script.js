51acc3601c1cb98f7fc79b2517a90ba7121de5eb
Amirhosein Azizi <77048259+purpamir@users.noreply.github.com>
2025-03-29 17:41:47 +0330

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ btnHold.addEventListener("click", function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 25) {
+    if (scores[activePlayer] >= 45) {
       playing = false; //to disable buttons
       document.querySelector(".tip").classList.add("hidden");
       document.querySelector(`.player--${activePlayer}`).classList.add("cup");
